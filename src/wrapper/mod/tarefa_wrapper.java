702075261c27eda63c8e75a737a0245a59115bff
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2023-11-19 21:36:30 -0300

    tarefa_wrapper

---
+++ src/wrapper/mod/tarefa_wrapper.java
@@ -0,0 +1,18 @@
+package wrapper.mod;
+
+public class tarefa_wrapper {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		Integer d = a;
+		
+		System.out.println("valor da conversão é " + d);
+		
+		
+		
+
+	}
+
+}
